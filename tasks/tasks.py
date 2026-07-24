4b0d41aa8e4119b9fdc324d2e3aa8d0716b76afd
RobertAvdeenkov <robertavdeenkov@gmail.com>
2026-07-24 13:40:30 +0300

    исправление багов

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -26,6 +26,8 @@ def reglog(data=Body(), db:Session=Depends(get_db)):
     repo=TasksRepositry(db)
     service=TaskService(repo)
     user=db.query(User).filter(User.name==data['name']).first()
+    if user:
+        print(user.password, type(user.password))
     try:
         if user is None:
             service.check_add_user(data['name'], data['password'])
